1d2e9c175e67fa11efc02b0e3c17f490f69a0931
slayfen <147399701+slayfen@users.noreply.github.com>
2024-02-01 23:35:24 +0200

    by botraikova

---
+++ src/sevenDigits.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class sevenDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter digit:");
+        int digit = scanner.nextInt();
+        switch (digit) {
+            case 1:
+                System.out.println("“The"+'\t' +digit + "-st day of the week is Monday”");
+                break;
+            case 2:
+                System.out.println("“The"+'\t' +digit + "-nd day of the week is Tuesday”");
+                break;
+            case 3:
+                System.out.println("“The"+'\t' +digit + " -rd day of the week is Wednesday”");
+                break;
+            case 4:
+                System.out.println("“The"+'\t' +digit + " -th day of the week is Thursday”");
+                break;
+            case 5:
+                System.out.println("“The"+'\t' +digit + " -th day of the week is Friday”");
+                break;
+            case 6:
+                System.out.println("“The"+'\t' +digit + "-th day of the week is Saturday”");
+                break;
+            case 7:
+                System.out.println("“The"+'\t' +digit + "-th day of the week is Sunday”");
+                break;
+        }
+    }
+
+}
