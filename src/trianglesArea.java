24535d975e9442626c464ef65cdac26df95964f4
slayfen <147399701+slayfen@users.noreply.github.com>
2024-02-01 22:46:13 +0200

    by botraikova

--- src/trianglesArea.java
+++ src/trianglesArea.java
@@ -2,8 +2,8 @@ public class trianglesArea {
 
     public static void main(String[] args) {
 
-        int height = 3;
-        int base = 4;
+        double height = 3;
+        double base = 4;
 
         double area = (height * base) / 2;
         System.out.println(area);
